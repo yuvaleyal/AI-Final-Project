41cc53a0f8d9d6312333fd895cdc6e73c1bb4a90
samharel <shmuel@reblaze.com>
2024-09-08 20:08:51 +0300

    now it works

--- PlayerFactory.py
+++ PlayerFactory.py
@@ -1,25 +1,30 @@
 import Player
 from Constants import BLACK, WHITE
+from DNNPlayer import DNNPlayer
+from FirstChoicePlayer import FirstChoicePlayer
 from HumanPlayer import HumanPlayer
 from MinimaxPlayer import MinimaxPlayer
 from RandomPlayer import RandomPlayer
+from ReinforcementPlayer import ReinforcementPlayer
 
 
 class PlayerFactory:
     @staticmethod
-    def get_player(player_type, player_num, display) -> Player:
+    def get_player(player_type, player_num, display=None) -> Player:
         # define the color of the new player:
-        color = WHITE if player_num == 1 else BLACK
+        color = player_num
         # Create the new player
-        if player_type == 'human':
-            return HumanPlayer(color, display)
-        elif player_type == 'random':
+        if player_type == 'random':
             return RandomPlayer(color)
         elif player_type == 'minimax':
             return MinimaxPlayer(color)
-        # elif player_type == 'rl':
-        #     return ReinforcedLearninigPlayer(color)
-        # elif player_type == 'neural_net':
-        #     return NeuralNetworkPlayer(color)
+        elif player_type == 'rl':
+            return ReinforcementPlayer(color)
+        elif player_type == 'dnn':
+            return DNNPlayer(color)
+        elif player_type == 'first_choice':
+            return FirstChoicePlayer(color)
+        elif player_type == 'human':
+            return HumanPlayer(color, display)
         return None
 

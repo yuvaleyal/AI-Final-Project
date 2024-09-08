fa9859f8f880df7670e169e8169e848dc35fa34e
Yuval Eyal <uveyal@gmail.com>
2024-09-08 12:34:31 +0300

    first choise palyer

---
+++ FirstChoisePlayer.py
@@ -0,0 +1,13 @@
+from Player import Player
+from State import State
+
+class FirstChoisePlayer(Player):
+    """this player makes moves by random"""
+
+    def __init__(self, color: int) -> None:
+        super().__init__(color)
+
+    def make_move(self, state: State) -> State:
+        options = state.find_all_moves()
+        move = options[0]
+        return state.next_state(move)
